8ff7ef56f5a9347d9c7dd0b7510fb9467ccbf818
Nick Escalona <nicholas.escalona@gmail.com>
2021-02-10 16:16:14 -0700

    visual studio demo and nuget

---
+++ 1-csharp/HelloVisualStudio/HelloVisualStudio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Humanizer;
+
+namespace HelloVisualStudio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inventory App 1.0");
+            Console.Write("What item do you have? ");
+            string item = Console.ReadLine(); // computer, apple
+            Console.Write("How many do you have? ");
+            int quantity = int.Parse(Console.ReadLine()); // 2, 6
+
+            if (quantity != 1)
+            {
+                item = Pluralize(item);
+            }
+
+            Console.WriteLine($"You have {quantity} {item}.");
+        }
+
+        static string Pluralize(string item)
+        {
+            return item.Pluralize();
+        }
+    }
+}
