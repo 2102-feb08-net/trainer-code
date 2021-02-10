f74f67264edbaffcb7514ffa031681532f80db11
Nick Escalona <nicholas.escalona@gmail.com>
2021-02-10 08:27:28 -0700

    add some notes on design

--- 1-csharp/StoreApp/Program.cs
+++ 1-csharp/StoreApp/Program.cs
@@ -5,6 +5,25 @@ namespace StoreApp
 {
     class Program
     {
+        // don't repeat yourself (DRY principle)
+        // separation of concerns
+        //     different classes, methods, projects, anything
+        //     should have their own distinct concerns, responsibilities
+        //  e.g. separate the classes for user input & output
+        //     from all the rest of the classes
+
+
+        // a project is a collection of classes (and other types)
+        // that will all be compiled together (into an assembly)
+
+        // roughly two kinds of projects -
+        //   ones that can run on their own (application projects, like console app)
+        //        (has a Main method)
+        //   ones that can't run on their own (library projects)
+        //        (doesn't have a Main method)
+        //
+
+
         static void Main(string[] args)
         {
             var products = new List<Product>();
