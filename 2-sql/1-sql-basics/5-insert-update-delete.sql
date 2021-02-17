9d4f387fe045d0a80aef461d1d629cca2c672604
Nick Escalona <nicholas.escalona@gmail.com>
2021-02-17 16:11:23 -0700

    add dml notes and some extra notes for tomorrow.

--- 2-sql/1-sql-basics/5-insert-update-delete.sql
+++ 2-sql/1-sql-basics/5-insert-update-delete.sql
@@ -56,7 +56,26 @@ WHERE GenreId >= 100;
 -- 2. insert two new records into the tracks table.
 
 -- 3. update customer Aaron Mitchell's name to Robert Walter
+UPDATE Customer
+SET FirstName = 'Robert', LastName = 'Walter'
+WHERE FirstName = 'Aaron' AND LastName = 'Mitchell';
 
 -- 4. delete one of the employees you inserted.
 
 -- 5. delete customer Robert Walter.
+DELETE FROM InvoiceLine
+WHERE InvoiceId IN (
+	SELECT InvoiceId
+	FROM Invoice
+	WHERE CustomerId = (
+		SELECT CustomerId FROM Customer WHERE FirstName = 'Robert' AND LastName = 'Walter'
+	)
+);
+
+DELETE FROM Invoice
+WHERE CustomerId = (
+	SELECT CustomerId FROM Customer WHERE FirstName = 'Robert' AND LastName = 'Walter'
+);
+
+DELETE FROM Customer
+WHERE FirstName = 'Robert' AND LastName = 'Walter';
