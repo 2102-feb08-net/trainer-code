6f41cac7100fc155fb240c9dfd08010d87775122
Nick Escalona <nicholas.escalona@gmail.com>
2021-03-09 15:59:03 -0700

    add integration testing with ef core / sqlite

---
+++ 5-soa/EmailApp/EmailApp.IntegrationTests/MessageRepositoryTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailApp.DataAccess;
+using EmailApp.DataAccess.EfModel;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace EmailApp.IntegrationTests
+{
+    public class MessageRepositoryTests
+    {
+        [Fact]
+        public void Get_GetsExistingEmail()
+        {
+            // arrange
+            using var contextFactory = new TestEmailContextFactory();
+            using EmailContext context = contextFactory.CreateContext();
+            // insert test data here, or maybe have a helper method somewhere to possibly share it between tests
+            var insertedEmail = new Message
+            {
+                Body = "asdf",
+                Date = new DateTimeOffset(2021, 1, 1, 0, 0, 0, new TimeSpan(0)),
+                From = new Account { Address = "from@from.com" },
+                Subject = "example subject"
+            };
+            context.Messages.Add(insertedEmail);
+            context.SaveChanges();
+            var repo = new MessageRepository(context);
+
+            // act
+            Business.Email email = repo.Get(insertedEmail.Id);
+
+            // assert
+            Assert.Equal(insertedEmail.Id, email.Id);
+            Assert.Equal(insertedEmail.Body, email.Body);
+            Assert.Equal(insertedEmail.Date, email.Sent);
+            Assert.Equal(insertedEmail.From.Address, email.From);
+            Assert.Equal(insertedEmail.Subject, email.Subject);
+        }
+        // should also test error scenarios
+
+        [Fact]
+        public void Create_CreateValidEmail()
+        {
+            // arrange
+            using var contextFactory = new TestEmailContextFactory();
+            using var context = contextFactory.CreateContext();
+            var from = new Account { Address = "from@from.com" };
+            context.Accounts.Add(from);
+            context.SaveChanges();
+            var emailToCreate = new Business.Email
+            {
+                Body = "asdf",
+                Sent = new DateTimeOffset(2021, 1, 1, 0, 0, 0, new TimeSpan(0)),
+                From = from.Address,
+                Subject = "example subject"
+            };
+            var repo = new MessageRepository(context);
+
+            // act
+            repo.Create(emailToCreate);
+            // (that method doesn't savechanges, so i need to use the same context instance to verify)
+
+            // assert
+            Message email = context.Messages.Local.Single(m => m.Date == emailToCreate.Sent);
+            Assert.Equal(emailToCreate.Id, email.Id);
+            Assert.Equal(emailToCreate.Body, email.Body);
+            Assert.Equal(emailToCreate.Sent, email.Date);
+            Assert.Equal(emailToCreate.From, email.From.Address);
+            Assert.Equal(emailToCreate.Subject, email.Subject);
+        }
+        // should also test error scenarios
+
+        // should also test "saving no changes"
+        [Fact]
+        public void Save_SavesOneCreate()
+        {
+            // arrange
+            using var contextFactory = new TestEmailContextFactory();
+            var from = new Account { Address = "from@from.com" };
+            var emailToCreate = new Business.Email
+            {
+                Body = "asdf",
+                Sent = new DateTimeOffset(2021, 1, 1, 0, 0, 0, new TimeSpan(0)),
+                From = from.Address,
+                Subject = "example subject"
+            };
+            using (var arrangeActContext = contextFactory.CreateContext())
+            {
+                arrangeActContext.Accounts.Add(from);
+                arrangeActContext.SaveChanges();
+                var repo = new MessageRepository(arrangeActContext);
+                repo.Create(emailToCreate);
+                // a email is ready to be saved, but not saved yet
+
+                // act
+                repo.Save();
+            }
+
+            // assert
+            using var assertContext = contextFactory.CreateContext();
+            Message email = assertContext.Messages.Include(m => m.From).Single(m => m.Date == emailToCreate.Sent);
+            Assert.Equal(emailToCreate.Body, email.Body);
+            Assert.Equal(emailToCreate.Sent, email.Date);
+            Assert.Equal(emailToCreate.From, email.From.Address);
+            Assert.Equal(emailToCreate.Subject, email.Subject);
+        }
+        // should also test error scenarios
+
+        // rule of thumb - have separate context instances for all three stages, arrange, act, and assert
+        // to ensure everything that should go all the way to/from the database does and nothing else
+    }
+}
---
+++ 5-soa/EmailApp/EmailApp.IntegrationTests/TestEmailContextFactory.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailApp.DataAccess.EfModel;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+
+namespace EmailApp.IntegrationTests
+{
+    public class TestEmailContextFactory : IDisposable
+    {
+        private DbConnection _connection;
+        private bool _disposedValue;
+
+        private DbContextOptions<EmailContext> CreateOptions()
+        {
+            return new DbContextOptionsBuilder<EmailContext>()
+                .UseSqlite(_connection).Options;
+        }
+
+        public EmailContext CreateContext()
+        {
+            if (_connection == null)
+            {
+                _connection = new SqliteConnection("DataSource=:memory:");
+                _connection.Open();
+
+                DbContextOptions<EmailContext> options = CreateOptions();
+                using var context = new EmailContext(options);
+                context.Database.EnsureCreated();
+
+                // add extra test seed data here (or, in each test method)
+            }
+
+            return new EmailContext(CreateOptions());
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _connection.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
