69ad5986554bc028a8ec719805a260c28f64ee22
Nick Escalona <nicholas.escalona@gmail.com>
2021-03-19 10:33:23 -0600

    asp.net core integration testing example

---
+++ 5-soa/EmailApp/EmailApp.IntegrationTests/HttpIntegrationTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using System.Net.Mime;
+using System.Text;
+using System.Threading.Tasks;
+using EmailApp.WebUI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Xunit;
+
+namespace EmailApp.IntegrationTests
+{
+    public class HttpIntegrationTests : IClassFixture<SqliteWebApplicationFactory<WebUI.Startup>>
+    {
+        private readonly HttpClient _client;
+        private readonly SqliteWebApplicationFactory<WebUI.Startup>
+            _factory;
+
+        public HttpIntegrationTests(SqliteWebApplicationFactory<WebUI.Startup> factory)
+        {
+            _factory = factory;
+            _client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = false
+            });
+        }
+
+        [Fact]
+        public async Task GetInbox_ValidJson()
+        {
+            HttpResponseMessage response = await _client.GetAsync("/api/inbox");
+            Assert.Equal(expected: HttpStatusCode.OK, actual: response.StatusCode);
+            // assert on any headers etc
+            Assert.StartsWith(MediaTypeNames.Application.Json, response.Content.Headers.ContentType.MediaType);
+
+            var messages = await response.Content.ReadFromJsonAsync<List<Message>>();
+            // relies on data in context HasData
+            Assert.Equal(2, messages.Count);
+            Assert.Equal("qc", messages[0].Subject);
+        }
+    }
+}
---
+++ 5-soa/EmailApp/EmailApp.IntegrationTests/SqliteWebApplicationFactory.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmailApp.DataAccess.EfModel;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace EmailApp.IntegrationTests
+{
+    public class SqliteWebApplicationFactory<TStartup>
+        : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        private readonly SqliteConnection _connection;
+
+        public SqliteWebApplicationFactory()
+        {
+            _connection = new SqliteConnection("DataSource=:memory:");
+            _connection.Open();
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                // get the entry the regular Startup method added from AddDbContext and replace it
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType == typeof(DbContextOptions<EmailContext>));
+                services.Remove(descriptor);
+
+                services.AddDbContext<EmailContext>(options =>
+                {
+                    options.UseSqlite(_connection);
+                });
+
+                var sp = services.BuildServiceProvider();
+
+                using (var scope = sp.CreateScope())
+                {
+                    var scopedServices = scope.ServiceProvider;
+                    var db = scopedServices.GetRequiredService<EmailContext>();
+
+                    db.Database.EnsureCreated();
+
+                    // if we needed initial data different from what the context has in HasData calls, then...
+                    //var logger = scopedServices
+                    //    .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+                    //try
+                    //{
+                    //    Utilities.InitializeDbForTests(db);
+                    //}
+                    //catch (Exception ex)
+                    //{
+                    //    logger.LogError(ex, "An error occurred seeding the " +
+                    //        "database with test messages. Error: {Message}", ex.Message);
+                    //}
+                }
+            });
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _connection.Close();
+        }
+    }
+}
