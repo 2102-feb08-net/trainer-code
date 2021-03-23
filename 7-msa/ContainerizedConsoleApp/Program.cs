7122afb1dd6310fa3fe12f39af183e6c6b3ef3b4
Nick Escalona <nicholas.escalona@gmail.com>
2021-03-23 12:59:57 -0600

    oops, i didn't commit the rest

---
+++ 7-msa/ContainerizedConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ContainerizedConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Docker!");
+        }
+    }
+}
